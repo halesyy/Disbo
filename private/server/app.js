d1e25ae26154cd0d6cde8a41b6cc5fa2bc825745
Jack Hales <halesyy@gmail.com>
2019-04-18 22:16:31 +1000

    Started removing bloatware comments

--- private/server/app.js
+++ private/server/app.js
@@ -1,28 +1,5 @@
-/**
- * Habbo HTML5
- *
- * Based upon original code by:
- *
- * Copyright (c) 2014 Kedi Agbogre (me@kediagbogre.com)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to
- * deal in the Software without restriction, including without limitation the
- * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
- * sell copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
- * IN THE SOFTWARE.
- */
+// Backend application
+
 var env = require("./h5/environment"),
 	conf  = require("./h5/conf.json"),
 	fuse  = require("./h5/fuserights.json"),
@@ -67,11 +44,12 @@ var callbacks = {
 	}
 };
 
-h5.scope().event.on("begin client connection", callbacks.clientConnection);
-h5.scope().event.on("begin sso check",         callbacks.ssoCheck);
-h5.scope().event.on("establish connection",    callbacks.establishConnection);
-h5.scope().event.on("user login",              callbacks.userLogin);
-h5.scope().event.on("client view rendered",    callbacks.clientViewRendered);
-h5.scope().event.on("load room",               callbacks.loadRoom);
+/*Event listener establishments.*/
+h5.event.on("begin client connection", callbacks.clientConnection);
+h5.event.on("begin sso check",         callbacks.ssoCheck);
+h5.event.on("establish connection",    callbacks.establishConnection);
+h5.event.on("user login",              callbacks.userLogin);
+h5.event.on("client view rendered",    callbacks.clientViewRendered);
+h5.event.on("load room",               callbacks.loadRoom);
 
 h5.listen();
