ddb5d719bf330e416e6cf1dfc3199d243905789f
Oliyy <under5zzs@gmail.com>
2019-05-03 02:56:59 +0200

    bot updates

--- bot/events/ready.js
+++ bot/events/ready.js
@@ -1,4 +1,3 @@
 module.exports = (client) => {
-  client.user.setUsername("LuxLife");
   console.log(`Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
 }
